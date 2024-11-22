3ad23ee66b1c341d2bb3022453ecd90641490d8a
Praise-Atadja <p.atadja@alustudent.com>
2024-11-22 05:16:15 +0200

    Update WelcomePage.jsx

---
+++ src/pages/WelcomePage.jsx
@@ -0,0 +1,18 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
+import LoginForm from "./LoginForm.jsx";
+
+const App = () => {
+	return (
+		<div className="flex-1 overflow-auto relative ">
+			{/* Left Section */}
+
+			{/* Right Section */}
+			<div>
+				<LoginForm />
+			</div>
+		</div>
+	);
+};
+
+export default App;
